4923ee3e1b68d694daa3d845e9616d288aff2a5d
moneshkannan <moneshkannan02@gmail.com>
2021-03-10 21:33:57 +0530

    updated

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyDTmkxpHoJtZGvx4XkpqooRpJjz1QGkj9M",
+    authDomain: "rio-ae27b.firebaseapp.com",
+    projectId: "rio-ae27b",
+    storageBucket: "rio-ae27b.appspot.com",
+    messagingSenderId: "76541471733",
+    appId: "1:76541471733:web:f1e1c7c7cb1ff1447ecebc",
+    measurementId: "G-T1EYKK8N7S"
+  };
